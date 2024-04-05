85c78a4de75d55dad3e58cb6f5b2f76dc0beff80
johndaniell@yahoo.com <johndaniell@yahoo.com>
2024-04-05 17:22:54 +0200

    LOL forgot about step 30 for testing purposes :D

--- javascript/index.js
+++ javascript/index.js
@@ -136,7 +136,7 @@ function displayBrusselsSproutsInstructions() {
     obtainInstruction('brusselsSprouts', 0),
     obtainInstruction('brusselsSprouts', 1),
     obtainInstruction('brusselsSprouts', 2),
-    obtainInstruction('brusselsSprouts', 30),
+    obtainInstruction('brusselsSprouts', 3),
     obtainInstruction('brusselsSprouts', 4),
     obtainInstruction('brusselsSprouts', 5),
     obtainInstruction('brusselsSprouts', 6),
